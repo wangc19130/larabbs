1a068a39abcfd1c0a428c8aec7ce9b8ea0dbc8e7
vagrant <vagrant@homestead>
2019-05-30 15:05:25 +0000

    add captcha

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -8,48 +8,49 @@ use Overtrue\EasySms\EasySms;
 
 class VerificationCodesController extends Controller
 {
-    public function store(VerificationCodeRequest $request,EasySms $easySms)
+
+    public function store(VerificationCodeRequest $request, EasySms $easySms)
     {
-        $phone = $request->phone;
+        $captchaData = \Cache::get($request->captcha_key);
+
+        if (!$captchaData) {
+            return $this->response->error('图片验证码已失效', 422);
+        }
+
+        if (!hash_equals($captchaData['code'], $request->captcha_code)) {
+            // 验证错误就清除缓存
+            \Cache::forget($request->captcha_key);
+            return $this->response->errorUnauthorized('验证码错误');
+        }
+
+        $phone = $captchaData['phone'];
+
         if (!app()->environment('production')) {
             $code = '1234';
-            $key = 'verificationCode_'.str_random(15);
-            $expiredAt = now()->addMinutes(10);
-            // 缓存验证码 10分钟过期。
-            \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
-            return $this->response->array([
-                'key' => $key,
-                'expired_at' => $expiredAt->toDateTimeString(),
-            ])->setStatusCode(201);
         } else {
             // 生成4位随机数，左侧补0
             $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+
             try {
                 $result = $easySms->send($phone, [
-                    'template' => 'SMS_97545008',
-                    'data' => [
-                        'code' => $code
-                    ],
+                    'content'  =>  "【Lbbs社区】您的验证码是{$code}。如非本人操作，请忽略本短信"
                 ]);
-                // $result = $easySms->send($phone, [
-                //     'content'  =>  "【Lbbs社区】您的验证码是{$code}。如非本人操作，请忽略本短信"
-                // ]);
             } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
-                $message = $exception->getException('aliyun')->getMessage();
-                return $this->response->errorInternal($message ?: '短信发送异常');
+                $message = $exception->getException('yunpian')->getMessage();
+                return $this->response->errorInternal($message ?? '短信发送异常');
             }
-
-            $key = 'verificationCode_'.str_random(15);
-            $expiredAt = now()->addMinutes(10);
-            // 缓存验证码 10分钟过期。
-            \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
-
-            return $this->response->array([
-                'key' => $key,
-                'expired_at' => $expiredAt->toDateTimeString(),
-            ])->setStatusCode(201);
         }
 
-    }
+        $key = 'verificationCode_'.str_random(15);
+        $expiredAt = now()->addMinutes(10);
+        // 缓存验证码 10分钟过期。
+        \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+        // 清除图片验证码缓存
+        \Cache::forget($request->captcha_key);
 
+        return $this->response->array([
+            'key' => $key,
+            'expired_at' => $expiredAt->toDateTimeString(),
+        ])->setStatusCode(201);
+    }
 }
--- app/Http/Requests/Api/VerificationCodeRequest.php
+++ app/Http/Requests/Api/VerificationCodeRequest.php
@@ -14,10 +14,18 @@ class VerificationCodeRequest extends FormRequest
     {
         return [
             'phone' => [
-                'required',
+                // 'required',
                 'regex:/^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199)\d{8}$/',
                 'unique:users'
             ]
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'captcha_key' => '图片验证码 key',
+            'captcha_code' => '图片验证码',
         ];
     }
 
